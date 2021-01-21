00a452afd99da463b9407662a82ceae8e95173d4
niraj <niraj.kumar@plexcel.com>
2021-01-21 17:56:45 +0530

    Added method for user service

--- src/app/features/users/users.service.ts
+++ src/app/features/users/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
@@ -11,50 +12,96 @@ import { Users } from './models/users.model';
 export class UsersService {
 
   baseUrl = `${environment.API_BASE_URL}/users`;
+  users: Users[] = [];
 
-  /**
-   * Setting headers if required
-   */
-
-  httpHeaders = new HttpHeaders({
-    'Content-type': 'application/json',
-  });
-
-  options = {
-    headers: this.httpHeaders,
-  };
-
-  constructor(private httpClient: HttpClient) { }
+  /*
+  
+  this is how we can also use rest call.
+  
+    httpHeaders = new HttpHeaders({
+      'Content-type': 'application/json',
+    });
+  
+    options = {
+      headers: this.httpHeaders,
+    };
+  
+    constructor(private httpClient: HttpClient) { }
+  
+    getUsers(): Observable<Users[]> {
+      const url = `${this.baseUrl}`;
+      return this.httpClient.get<Users[]>(url, this.options);
+    }
+  
+    getUserByID(id: number): Observable<Users[]> {
+      const user: Users[] = []
+      const url = `${this.baseUrl}/${id}`;
+      return this.httpClient.get<Users>(url, this.options).pipe(
+        map(post => {
+          user.push(post);
+          return user;
+        })
+      );
+    }
+  
+    createUser(user: Users): Observable<any> {
+      const url = `${this.baseUrl}`;
+      return this.httpClient.post<any>(url, user, this.options);
+    }
+  
+    updateUser(user: Users): Observable<any> {
+      const url = `${this.baseUrl}/${user.id}`;
+      return this.httpClient.put<any>(url, user, this.options);
+    }
+  
+  
+    deleteUser(id: number): Observable<any> {
+      const url = `${this.baseUrl}/${id}`;
+      return this.httpClient.delete<any>(url, this.options)
+    }
+  
+    */
 
   getUsers(): Observable<Users[]> {
-    const url = `${this.baseUrl}`;
-    return this.httpClient.get<Users[]>(url, this.options);
+    return of(this.users);
   }
 
-  getUserByID(id: number): Observable<Users[]> {
-    const user: Users[] = []
-    const url = `${this.baseUrl}/${id}`;
-    return this.httpClient.get<Users>(url, this.options).pipe(
-      map(post => {
-        user.push(post);
-        return user;
-      })
-    );
+  getUserByUserName(userName: string): Observable<Users> {
+    const user = this.users.find(user => user.username === userName);
+    if (!user) {
+      return of(null);
+    }
+    return of(user);
   }
 
   createUser(user: Users): Observable<any> {
-    const url = `${this.baseUrl}`;
-    return this.httpClient.post<any>(url, user, this.options);
+    if (user) {
+      this.users.push(user);
+    }
+    console.log(this.users);
+    return of({ createdUser: true });
   }
 
   updateUser(user: Users): Observable<any> {
-    const url = `${this.baseUrl}/${user.id}`;
-    return this.httpClient.put<any>(url, user, this.options);
+    const isUser = this.users.find(x => x.username === user.username);
+    if (!isUser) {
+      return of(null);
+    }
+    isUser.email = user.email;
+    isUser.name = user.name;
+    isUser.username = user.username
+    return of({ updatedUser: true });
+
   }
 
 
-  deleteUser(id: number): Observable<any> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.httpClient.delete<any>(url, this.options)
+  deleteUser(userName: string): Observable<any> {
+    const index = this.users.findIndex(x => x.username === userName);
+    if (index < 0) {
+      return of(null);
+    }
+    this.users.splice(index, 1);
+    return of({ isDeleted: true })
   }
+
 }
