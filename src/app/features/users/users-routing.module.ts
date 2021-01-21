a93b17ae609c9b7a0871255385bd995d0251db86
niraj <niraj.kumar@plexcel.com>
2021-01-21 14:33:34 +0530

    Added route for listing , create, edit and user details

--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './pages/add-user/add-user.component';
+import { ListOfUserComponent } from './pages/list-of-user/list-of-user.component';
+import { UserDetailsComponent } from './pages/user-details/user-details.component';
 
 import { UsersPage } from './users.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: UsersPage
+    component: UsersPage,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      { path: 'users', component: ListOfUserComponent },
+      { path: 'create', component: AddUserComponent },
+      { path: 'edit/:id', component: AddUserComponent },
+      { path: ':id', component: UserDetailsComponent },
+
+    ]
   }
 ];
 
@@ -14,4 +25,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class UsersPageRoutingModule {}
+export class UsersPageRoutingModule { }
