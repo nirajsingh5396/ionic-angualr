cc8277223d82bd6f8efc55b5d1ce9b8237d84f7d
niraj <niraj.kumar@plexcel.com>
2021-01-21 17:54:03 +0530

    Implemented add user

--- src/app/features/users/pages/add-user/add-user.component.ts
+++ src/app/features/users/pages/add-user/add-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Users } from '../../models/users.model';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,8 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  createUserForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UsersService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    console.log('add-user');
+    this.buildCreateUserForm();
+  }
+
+  buildCreateUserForm() {
+    this.createUserForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      username: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+    });
+  }
+
+  userCreateFormSubmission() {
+
+    if (!this.createUserForm.valid) {
+      return;
+    }
+    const user = this.createUserForm.value as Users;
+    this.userService.createUser(user)
+      .subscribe((res) => {
+        if (res.createdUser) {
+          this.router.navigate(['/users/users'])
+        }
+      });
+  }
+
+  formInputIsRequired(formInput: string): boolean {
+    if (this.createUserForm.controls[formInput]) {
+      if (this.createUserForm.controls[formInput].hasError('required')) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 
 }
