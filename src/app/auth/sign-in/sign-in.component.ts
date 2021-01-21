87203403cb52b383cede7077310fd31330af2f1a
niraj <niraj.kumar@plexcel.com>
2021-01-21 14:02:07 +0530

    Created sign in component, designed and implemented

---
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { SignInContext } from '../models/sign-in.model';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss'],
+})
+export class SignInComponent implements OnInit {
+
+  authSignInForm: FormGroup;
+  isShowErrorMessage: boolean = false;
+  errorMessage: string;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.buildSigninForm();
+  }
+
+  buildSigninForm() {
+    this.authSignInForm = this.formBuilder.group({
+      userId: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  authSignFormSubmission() {
+
+    if (!this.authSignInForm.valid) {
+      return;
+    }
+    const signnContext = this.authSignInForm.value as SignInContext;
+    this.authService.signIn(signnContext)
+      .subscribe((signInContextRes) => {
+        if (signInContextRes.authenticated) {
+
+        } else {
+          this.isShowErrorMessage = true;
+          this.errorMessage = signInContextRes.message;
+        }
+      });
+  }
+
+  formInputIsRequired(formInput: string): boolean {
+    if (this.authSignInForm.controls[formInput]) {
+      if (this.authSignInForm.controls[formInput].hasError('required')) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+}
